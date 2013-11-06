ba78f8dec9c86eb82e0f654d098bed372ac214b3
Jereem <jereeem18@gmail.com>
2013-11-06 14:00:23 +0100

    first commit

--- BE3/src/Game_Board/Board.java
+++ BE3/src/Game_Board/Board.java
@@ -3,12 +3,12 @@ package Game_Board;
 import Jeu_de_plateau.Game;
 
 public class Board {
-	protected int _nbCase = 0;
-	private Game _game;
-	public CheckerBoard _unnamed_CheckerBoard_;
-	public Game _unnamed_Game_;
-	public LineBoard _unnamed_LineBoard_;
-	protected Game_Piece _unnamed_Game_Piece_;
+	protected int nbCase = 0;
+	private Game game;
+	public CheckerBoard unnamed_CheckerBoard_;
+	public Game unnamed_Game_;
+	public LineBoard unnamed_LineBoard_;
+	protected Game_Piece unnamed_Game_Piece_;
 
 	public Board(int aNbCase) {
 		throw new UnsupportedOperationException();
