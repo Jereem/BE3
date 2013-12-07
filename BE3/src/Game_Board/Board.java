b0ff32ec1fc394c007c170ca8ac36d58fe3a4267
teddy.delavallee <teddy.delavallee@gmail.com>
2013-12-07 20:32:48 +0100

    commit ted

--- BE3/src/Game_Board/Board.java
+++ BE3/src/Game_Board/Board.java
@@ -6,19 +6,32 @@ public class Board {
 	protected int nbBox;
 	protected Arraylist<Game_Piece> gamePieces = new ArrayList<Game_Piece>();
         
-	public Board(int aNbCase) {
-		throw new UnsupportedOperationException();
+        public Board() {
+	}
+        
+	public Board(int pNbBox) {
+		this.nbBox=pNbBox;
 	}
 
 	public int getNbBox() {
-		throw new UnsupportedOperationException();
+		return this.nbBox;
 	}
 
-	public void setNbBox(int aPNbCase) {
-		throw new UnsupportedOperationException();
+	public void setNbBox(int pNbBox) {
+		this.nbBox=pNbBox;
 	}
 
-	public void clear() {
-		throw new UnsupportedOperationException();
-	}
+	public void addGamePiece(Game_Piece pPiece){
+            gamePieces.add(pPiece);
+        }
+        
+        public void delGamePiece(Game_Piece pPiece){
+            if(gamePieces.indexOf(pPiece) != -1){
+                gamePieces.remove(gamePieces.indexOf(pPiece));
+            }
+        }
+            
+        public void delGamePiece(int nbPiece){
+                gamePieces.remove(nbPiece);
+        }
 }
